1cd37d128ab61629cb487cb9aa25fb4f3f0ceccb
octobertech <octobrtech@gmail.com>
2020-11-17 23:30:59 +0600

    initial commit

--- src/main.js
+++ src/main.js
@@ -3,4 +3,19 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
-createApp(App).use(store).use(router).mount('#app')
+
+
+// Importing the Nova PrimeVue theme styles
+import 'primevue/resources/themes/nova/theme.css';
+
+// Importing the base PrimeVue component styles
+import 'primevue/resources/primevue.min.css';
+
+// Importing the base PrimeIcon styles
+import 'primeicons/primeicons.css';
+
+import 'primeflex/primeflex.css';
+
+const app = createApp(App)
+app.config.globalProperties.$primevue = {ripple: true};
+app.use(store).use(router).mount('#app')
